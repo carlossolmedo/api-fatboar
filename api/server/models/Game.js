352d66177cbcd4e9bec4899e26869d6bbc8c4494
Carlos <carlos.olmedodev@gmail.com>
2020-12-16 17:48:36 +0100

    feat(api, models): Schema models added

---
+++ api/server/models/Game.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const gameSchema = new mongoose.Schema({
+    user_id: String,
+    game_count: Number,
+    date_created: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Game', gameSchema);
