f6b7bcd1e069452019e33bf43d9ec35b56cbe688
Carlos <carlos.olmedodev@gmail.com>
2021-01-29 12:59:40 +0100

    feat(client, homepage): Vuelidate added, verification fields sign up - css modal updated

--- client/components/Auth/ModalAuth.vue
+++ client/components/Auth/ModalAuth.vue
@@ -8,23 +8,23 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 18 18">
               <path
                 d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
-              </svg>
+            </svg>
           </a>
         </div>
         <div class="modal__body">
           <div class="c-fluid-container connection-title">
             <div class="c-row">
-              <button id="btn-signUpForm" @click="showForm('signUpForm')" class="btn btn-signUp active" type="button">s'inscrire</button>
-              <button id="btn-loginForm" @click="showForm('loginForm')" class="btn btn-login" type="button">connection</button>
+              <button id="btn-signUpForm" @click="showForm('signUpForm')" class="btn btn-signUp active"
+                type="button">s'inscrire</button>
+              <button id="btn-loginForm" @click="showForm('loginForm')" class="btn btn-login"
+                type="button">connection</button>
             </div>
           </div>
-          <!-- Sign Up -->
           <div id="signUpForm" class="form-block signUp__block show">
             <div class="c-fluid-container">
               <SignUp />
             </div>
           </div>
-          <!-- Login -->
           <div id="loginForm" class="form-block login__block">
             <div class="c-fluid-container">
               <Login />
@@ -79,71 +79,6 @@
 </script>
 
 <style lang="scss">
-  .modal {
-    overflow-x: hidden;
-    overflow-y: auto;
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 9;
-
-    &__backdrop {
-      background-color: rgba(0, 0, 0, 0.3);
-      position: fixed;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      z-index: 1;
-    }
-
-    &__dialog {
-      background-color: #ffffff;
-      position: relative;
-      width: 600px;
-      margin: 50px auto;
-      display: flex;
-      flex-direction: column;
-      border-radius: 5px;
-      z-index: 2;
-
-      @media screen and (max-width: 992px) {
-        width: 90%;
-      }
-    }
-
-    &__close {
-      cursor: pointer;
-      opacity: 0.3;
-
-      &:hover {
-        opacity: 1;
-        transition: all .5s ease-in-out;
-      }
-    }
-
-    &__header {
-      padding: 20px 20px 10px;
-      display: flex;
-      align-items: center;
-      justify-content: right;
-    }
-
-    &__body {
-      padding: 10px 20px 10px;
-      overflow: auto;
-      display: flex;
-      flex-direction: column;
-      align-items: stretch;
-    }
-
-    &__footer {
-      padding: 10px 20px 20px;
-    }
-  }
-
   .fade-enter-active,
   .fade-leave-active {
     transition: opacity 0.2s;
@@ -152,50 +87,6 @@
   .fade-enter,
   .fade-leave-to {
     opacity: 0;
-  }
-
-// Form styles
-
-  .connection-title {
-    margin-bottom: 1.5rem;
-
-    .btn {
-      width: calc(100%/2);
-      padding: 1rem;
-      font-weight: bold;
-      letter-spacing: .15rem;
-      text-transform: uppercase;
-      cursor: pointer;
-
-      &:hover {
-        background-color: #111;
-        color: #ffffff;
-        transition: all .3s ease;
-      }
-
-      &-signUp {
-        background-color: transparent;
-        color: #111;
-        border-color: transparent transparent #1c374d;
-        border-right: none;
-        // border-color: #1c374d #1c374d transparent;
-      }
-      &-login {
-        background-color: transparent;
-        color: #111;
-        border-color: transparent transparent #1c374d;
-        border-left: none;
-      }
-    }
-    .active {
-      background-color: #111;
-      color: #ffffff;
-      border: none;
-    }
-  }
-
-  .show {
-    display: block !important;
   }
 
   .signUp__block {
@@ -209,15 +100,16 @@
   form.connection {
     width: 100%;
 
-    & > dl {
+    & dl {
       text-align: left;
       margin-bottom: 1rem;
 
-      & > dt {
+      &>dt {
         display: block;
         margin-bottom: .5rem;
       }
     }
+
     .btn-block {
       margin: 1.5rem 0 1.5rem 0;
     }
@@ -225,6 +117,6 @@
 
   .third-connection-block {
     width: 100%;
-    margin: 1.5rem 0 1.5rem 0;
+    margin: 1rem 0 1.5rem 0;
   }
 </style>
--- client/components/Auth/SignUp.vue
+++ client/components/Auth/SignUp.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="c-row">
-    <form class="connection" action="/" method="get">
+    <form @submit.prevent="submit" class="connection">
+      <dl>
+        <dt>
+          <span>Genre</span>
+        </dt>
+        <dd>
+          <label>
+            <input type="radio" name="gender" value="M" autofocus="autofocus" class="c-form-control"> M.
+          </label>
+          <label>
+            <input type="radio" name="gender" value="F" autofocus="autofocus" class="c-form-control"> Mme.
+          </label>
+        </dd>
+      </dl>
       <dl class="required">
         <dt>
           <label for="name">Nom</label>
         </dt>
         <dd>
-          <input type="text" id="name" autofocus="autofocus" class="c-form-control-input" required>
+          <input v-model.trim="$v.name.$model" type="text" name="name" id="name" autofocus="autofocus"
+            class="c-form-control-input" :class="{'input-error': $v.name.$error}" autocomplete="off" required>
+          <small class="error" v-if="!$v.name.minLength">Votre nom doit contenir {{$v.name.$params.minLength.min}}
+            lettres minimum.</small>
         </dd>
       </dl>
       <dl class="required">
@@ -14,31 +30,83 @@
           <label for="email">Adresse mail</label>
         </dt>
         <dd>
-          <input type="email" id="email" autofocus="autofocus" class="c-form-control-input" required>
+          <input type="email" name="email" id="email" autofocus="autofocus" class="c-form-control-input" required>
         </dd>
       </dl>
+      <dl class="required">
+        <dt>
+          <label for="birthdate">Date de naissance</label>
+        </dt>
+        <dd>
+          <input type="date" placeholder="jj-mm-aaaa" min="1997-01-01" max="2030-12-31" name="birthdate" id="birthdate"
+            class="c-form-control-input" required>
+        </dd>
+      </dl>
+      <div class="c-row c-row--between">
+        <div class="c-row__col c-row__col--1-1 c-row__col--md-1-3">
+          <dl class="required">
+            <dt>
+              <label for="postal_code">Code postal</label>
+            </dt>
+            <dd>
+              <input v-model.trim="$v.postalCode.$model" type="text" name="postal_code" id="postal_code"
+                autofocus="autofocus" class="c-form-control-input" :class="{'input-error': $v.postalCode.$error}" maxlength="5" required>
+              <small class="error" v-if="$v.postalCode.$error">Code postal invalide.</small>
+            </dd>
+          </dl>
+        </div>
+        <div class="c-row__col c-row__col--1-1 c-row__col--md-1-2">
+          <dl class="required">
+            <dt>
+              <label for="country">Pays</label>
+            </dt>
+            <dd>
+              <div class="c-select">
+                <select @change="setCountry($event.target.value)" class="c-select__control" :class="{'input-error': messageCountry}" id="country" name="country"
+                  required>
+                  <option value="FR">France</option>
+                  <option value="other">Autre</option>
+                </select>
+                <span class="c-select__toggle"></span>
+              </div>
+              <div>
+                <small class="error" v-if="messageCountry">Jeu uniquement en France metropolitenne.</small>
+              </div>
+            </dd>
+          </dl>
+        </div>
+      </div>
       <dl class="required">
         <dt>
           <label for="password">Password</label>
         </dt>
         <dd>
-          <input type="password" id="password" autofocus="autofocus" class="c-form-control-input" required>
+          <input v-model.trim="$v.password.$model" type="password" name="password" id="password" autofocus="autofocus"
+            class="c-form-control-input" :class="{'input-error': $v.password.$error}" required>
+          <small class="error" v-if="!$v.password.minLength">Votre mot de passe doit contenir
+            {{$v.password.$params.minLength.min}} characteres minimum.</small>
         </dd>
       </dl>
       <dl>
         <dt>
           <label for="cdg">
-            <input type="checkbox" id="cdg" autofocus="autofocus" class="c-form-control" required> J'accepte les <a
-              href="#cdg">conditions génerales</a>
+            <input type="checkbox" name="cdg" autofocus="autofocus" id="cdg" class="c-form-control" required>
+            J'accepte les <a href="#cdg">conditions générales</a>
           </label>
         </dt>
       </dl>
       <div class="btn-block">
-        <input type="submit" class="c-btn c-btn--block c-btn--primary" value="Je m'inscris">
+        <button type="submit" :disabled="$v.validationGroup.$invalid" class="c-btn c-btn--block c-btn--primary">
+          Je m'inscris
+        </button>
+        <!-- <button type="submit" :disabled="submitStatus === 'PENDING'" class="c-btn c-btn--block c-btn--primary">Je m'inscris</button> -->
+        <p class="typo__p" v-if="submitStatus === 'OK'">Thanks for your submission!</p>
+        <p class="typo__p" v-if="submitStatus === 'ERROR'">Please fill the form correctly.</p>
+        <p class="typo__p" v-if="submitStatus === 'PENDING'">Sending...</p>
       </div>
     </form>
     <div class="third-connection-block">
-      <h3>Ou</h3>
+      <h5><span class="hyphen">Ou</span></h5>
       <ul class="c-navbar c-navbar--center">
         <li class="c-navbar__item"><a href="#google">Google</a></li>
         <li class="c-navbar__item"><a href="#fb">Facebook</a></li>
@@ -46,3 +114,64 @@
     </div>
   </div>
 </template>
+
+<script>
+  import {
+    required,
+    minLength,
+    maxLength,
+    numeric
+  } from 'vuelidate/lib/validators';
+
+  export default {
+    data() {
+      return {
+        name: '',
+        password: '',
+        postalCode: '',
+        messageCountry: false,
+        submitStatus: null,
+        btnDisabled: ''
+      }
+    },
+    validations: {
+      name: {
+        required,
+        minLength: minLength(6)
+      },
+      password: {
+        required,
+        minLength: minLength(8)
+      },
+      postalCode: {
+        required,
+        numeric,
+        minLength: minLength(5),
+        maxLength: maxLength(5)
+      },
+      validationGroup: ['name', 'password']
+    },
+    methods: {
+      setCountry(value) {
+        if (value === 'other') {
+          this.messageCountry = true;
+        } else {
+          this.messageCountry = false;
+        }
+      },
+      submit() {
+        this.$v.$touch();
+        if (this.$v.$invalid) {
+          this.submitStatus = 'ERROR';
+        } else {
+          // do your submit logic here
+          this.submitStatus = 'PENDING';
+          setTimeout(() => {
+            this.submitStatus = 'OK';
+            this.btnDisabled = false;
+          }, 500);
+        }
+      }
+    }
+  }
+</script>
