53de6a89ec35b8b5cac67cb80103f3710d29e36d
Carlos <carlos.olmedodev@gmail.com>
2021-02-01 12:14:23 +0100

    feat(client): New pages for footer and links added

--- client/components/Footer.vue
+++ client/components/Footer.vue
@@ -2,9 +2,9 @@
   <footer>
     <div class="c-container">
       <ul class="c-navbar c-navbar--center">
-        <li class="c-navbar__item"><a class="footer__link" href="#cdg">Condition Générales</a></li>
-        <li class="c-navbar__item"><a class="footer__link" href="#mentions">Mention Légal</a></li>
-        <li class="c-navbar__item"><a class="footer__link" href="#dp">Données Personnelles</a></li>
+        <li class="c-navbar__item"><NuxtLink to="/conditions" class="footer__link">Condition Générales</NuxtLink></li>
+        <li class="c-navbar__item"><NuxtLink to="/terms" class="footer__link" href="#mentions">Mention Légal</NuxtLink></li>
+        <li class="c-navbar__item"><NuxtLink to="/privacy" class="footer__link" href="#dp">Données Personnelles</NuxtLink></li>
       </ul>
       <div class="u-text-center">&copy; Copyright Fatboar 2020</div>
     </div>
---
+++ client/pages/terms.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="c-container">
+    <h1>Mention Légal</h1>
+    <section>
+      <h2>Point #2</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam tempore nisi sunt magnam incidunt deleniti laudantium beatae consequatur a perferendis, inventore reprehenderit ipsa quos vel eaque? Fuga eos eius dolore?
+      Sapiente exercitationem asperiores assumenda voluptas, a corporis iure doloremque. Pariatur, consequatur! Fugiat assumenda soluta minima cumque dolorum repellendus natus, molestias illo officia. Beatae exercitationem id ea nemo, dolores nostrum deserunt.
+      Blanditiis dolorem accusantium neque fugiat nesciunt, perspiciatis tempore fuga asperiores eos veritatis vel voluptates officiis placeat reiciendis quasi, nam omnis. Consequatur velit eaque magni eveniet quisquam facere repellendus eos aliquid.</p>
+    </section>
+    <section>
+      <h2>Point #3</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam tempore nisi sunt magnam incidunt deleniti laudantium beatae consequatur a perferendis, inventore reprehenderit ipsa quos vel eaque? Fuga eos eius dolore?
+      Sapiente exercitationem asperiores assumenda voluptas, a corporis iure doloremque. Pariatur, consequatur! Fugiat assumenda soluta minima cumque dolorum repellendus natus, molestias illo officia. Beatae exercitationem id ea nemo, dolores nostrum deserunt.
+      Blanditiis dolorem accusantium neque fugiat nesciunt, perspiciatis tempore fuga asperiores eos veritatis vel voluptates officiis placeat reiciendis quasi, nam omnis. Consequatur velit eaque magni eveniet quisquam facere repellendus eos aliquid.</p>
+    </section>
+  </div>
+</template>
