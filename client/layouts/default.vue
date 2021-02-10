9ad57a3a84d85c297caba2cc0720ebfd9c636c7b
Carlos <carlos.olmedodev@gmail.com>
2021-02-10 12:28:10 +0100

    feat(client, gdpr): CSS and texts added

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -3,21 +3,25 @@
     <Header />
     <Nuxt />
     <Footer :pageIndex="pageIndex" />
+    <GDPR :cookieAccepted="cookieAccepted" />
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header';
 import Footer from '~/components/Footer';
+import GDPR from '~/components/GDPR';
 
 export default {
   components: {
     Header,
-    Footer
+    Footer,
+    GDPR
   },
   data() {
     return {
-      pageIndex: false
+      pageIndex: false,
+      cookieAccepted: false
     }
   },
   created() {
@@ -27,36 +31,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-  cursor: pointer;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
