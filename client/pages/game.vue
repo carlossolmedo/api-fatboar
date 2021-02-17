e94f5aa5a5b66e36c96e4efa489f7904fc4a99cf
Carlos <carlos.olmedodev@gmail.com>
2021-02-17 15:38:10 +0100

    feat(client, footer): Added game style

--- client/pages/game.vue
+++ client/pages/game.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="c-container">
-    <div class="c-row">
-      <h1 class="u-text-center">Game</h1>
+  <main class="content">
+    <div class="bg-purple"></div>
+    <div class="c-container text-container">
+      <div class="c-row c-row--center c-row--middle">
+        <form>
+          <input type="text" name="ticket-number" placeholder="xxxxxxxxxx">
+          <div>
+            <button class="c-btn c-btn--info">Valider</button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 <script>
 export default {
