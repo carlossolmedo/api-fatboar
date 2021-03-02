80e98ab1bd875d4384df48f1bfd3f3f2bf19758c
Carlos <carlos.olmedodev@gmail.com>
2021-03-02 17:45:51 +0100

    feat(client): Loader game added - handle tickets errors done.

---
+++ client/components/LoaderGame.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="loading">
+    <img class="img-logo-game" src="~/assets/images/logo-fatboar.svg" alt="logo FatBoar">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: { type: Boolean }
+  }
+}
+</script>
--- client/pages/game.vue
+++ client/pages/game.vue
@@ -2,10 +2,10 @@
   <div>
     <div class="bg-purple"></div>
     <main class="content-game">
-      <Loader :loading="loading" />
+      <LoaderGame :loading="loading" />
       <div v-if="!loading && !submitStatus" class="block-game">
         <h1 class="title-game">Entrez votre numéro de ticket</h1>
-        <form @submit.prevent="submitGame" class="form-game">
+        <form id="formGame" @submit.prevent="submitGame" class="form-game">
           <span class="input">
             <input v-model.trim="$v.ticketNumber.$model" id="ticketNumber" class="input-game" type="text" name="ticketNumber"
               placeholder="256xxxxxxx" maxlength="10">
@@ -16,8 +16,13 @@
         </form>
       </div>
       <div v-if="submitStatus" class="ticket-winner">
-        <h3>Vous avez gagnez</h3>
-        <h1>{{ prize }} !</h1>
+        <h3>Vous avez gagné</h3>
+        <h1>🎉 {{ prize }} ! 🎉</h1>
+        <p>Merci d'avoir participé</p>
+        <p>Rendez-vous chez votre restaurant préféré à bientôt 😋</p>
+        <div class="c-btn-play" style="margin-top: 20px;">
+          <button @click="restart" class="btn-restart">Rejouer</button>
+        </div>
       </div>
     </main>
   </div>
@@ -28,12 +33,12 @@
     maxLength,
     numeric
   } from 'vuelidate/lib/validators';
-  import Loader from '../components/Loader';
+  import LoaderGame from '../components/LoaderGame';
 
   export default {
     middleware: 'auth',
     components: {
-      Loader
+      LoaderGame
     },
     data() {
       return {
@@ -51,6 +56,11 @@
       }
     },
     methods: {
+      restart() {
+        this.loading = false;
+        this.ticketNumber = '';
+        this.submitStatus = false;
+      },
       async submitGame() {
         this.loading = true;
         this.$v.$touch()
@@ -62,9 +72,19 @@
                 this.loading = false;
                 this.submitStatus = true;
                 this.prize = ticketWinner.type
-              }, 2000)
-          }).catch(() => {
-            this.$toast.error("Vérifiez votre numero de ticket").goAway(3000);
+              }, 3000)
+          }).catch((error) => {
+            if (error.response) {
+              if (error.response.status === 404) {
+                this.$toast.error("Le numéro de ticket n'existe pas").goAway(4000);
+              }
+              if (error.response.status === 401) {
+                this.$toast.error("Numero de ticket déjà joué").goAway(4000);
+              }
+            } else {
+              this.$toast.error("Vérifiez votre numéro de ticket").goAway(4000);
+            }
+            this.restart();
           });
         }
       }
