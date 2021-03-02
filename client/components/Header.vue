5835c83b3b29659a831a4cdd4c59d4b33f235632
Carlos <carlos.olmedodev@gmail.com>
2021-03-02 12:18:51 +0100

    feat(client): Validation ticket number and get data

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -4,13 +4,7 @@
       <div class="c-row c-row--middle">
         <div class="c-row_col c-row__col--auto">
           <div class="c-site-branding">
-              <NuxtLink v-if="!$auth.loggedIn" to="/" class="c-site-branding__title">
-                <img class="img-logo" src="~/assets/images/logo-fatboar.svg" alt="logo FatBoar">
-              </NuxtLink>
-              <NuxtLink v-if="$auth.loggedIn && $auth.user.role === 'customer'" to="/" class="c-site-branding__title">
-                <img class="img-logo" src="~/assets/images/logo-fatboar.svg" alt="logo FatBoar">
-              </NuxtLink>
-              <NuxtLink v-if="$auth.loggedIn && $auth.user.role === 'admin'" to="/admin/dashboard" class="c-site-branding__title">
+              <NuxtLink to="/" class="c-site-branding__title">
                 <img class="img-logo" src="~/assets/images/logo-fatboar.svg" alt="logo FatBoar">
               </NuxtLink>
           </div>
--- client/pages/game.vue
+++ client/pages/game.vue
@@ -2,16 +2,22 @@
   <div>
     <div class="bg-purple"></div>
     <main class="content-game">
-      <div class="block-game">
+      <Loader :loading="loading" />
+      <div v-if="!loading && !submitStatus" class="block-game">
+        <h1 class="title-game">Entrez votre numéro de ticket</h1>
         <form @submit.prevent="submitGame" class="form-game">
           <span class="input">
-            <input v-model.trim="$v.ticketNumber.$model" class="input-game" type="text" name="ticket-number"
+            <input v-model.trim="$v.ticketNumber.$model" id="ticketNumber" class="input-game" type="text" name="ticketNumber"
               placeholder="256xxxxxxx" maxlength="10">
           </span>
           <div class="c-btn-play">
-            <a class="btn-play" type="submit" :disabled="ticketNumberComplete">Jouer</a>
+            <button class="btn-play" type="submit" :disabled="$v.ticketNumber.$invalid">Jouer</button>
           </div>
         </form>
+      </div>
+      <div v-if="submitStatus" class="ticket-winner">
+        <h3>Vous avez gagnez</h3>
+        <h1>{{ prize }} !</h1>
       </div>
     </main>
   </div>
@@ -19,32 +25,47 @@
 <script>
   import {
     required,
-    maxLength
+    maxLength,
+    numeric
   } from 'vuelidate/lib/validators';
+  import Loader from '../components/Loader';
 
   export default {
     middleware: 'auth',
+    components: {
+      Loader
+    },
     data() {
       return {
-        ticketNumberComplete: true,
-        ticketNumber: null
+        ticketNumber: null,
+        submitStatus: false,
+        loading: false,
+        prize: null
       }
     },
     validations: {
       ticketNumber: {
         required,
+        numeric,
         maxLength: maxLength(10)
       }
     },
     methods: {
-      submitGame() {
-        console.log('submitGame launched');
-        this.$v.touch();
-        if (!this.$v.invalid) {
-          this.ticketNumberComplete = true;
-          setTimeout(() => {
-            console.log('form send', this.ticketNumber);
-          }, 2000)
+      async submitGame() {
+        this.loading = true;
+        this.$v.$touch()
+        if (!this.$v.$invalid) {
+          await this.$axios.$post('/tickets/verify', {
+            ticket_number: this.ticketNumber
+          }).then((ticketWinner) => {
+              setTimeout(() => {
+                this.loading = false;
+                this.submitStatus = true;
+                this.prize = ticketWinner.type
+              }, 2000)
+          }).catch(() => {
+            this.$toast.error("Vérifiez votre numero de ticket").goAway(3000);
+          });
         }
       }
     }
