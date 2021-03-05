c3979b123065457202404b86fd20b9ecf6342312
Carlos <carlos.olmedodev@gmail.com>
2021-03-05 11:59:03 +0100

    feat(client, prizes): Table styles done - Directory utils added, dateFormat script

--- client/pages/account/prizes.vue
+++ client/pages/account/prizes.vue
@@ -14,12 +14,12 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(ticket, index) in tickets">
+            <tr v-for="(ticket, index) in tickets" :key="ticket.ticket_number">
               <th>{{ index + 1 }}</th>
               <td>{{ ticket.ticket_number }}</td>
               <td>{{ ticket.type }}</td>
-              <td>{{ ticket.date_created }}</td>
-              <td>{{ ticket.received }}</td>
+              <td>{{ ticket.date_created | date }}</td>
+              <td><span class="received" :class="{'success': ticket.received, 'not-yet': !ticket.received}"></span></td>
             </tr>
           </tbody>
         </table>
@@ -29,11 +29,20 @@
 </template>
 
 <script>
+import dateFormat from '../../utils/dateFormat';
+
 export default {
   async asyncData({ $axios, $auth }) {
     const tickets = await $axios.$get(`/users/${$auth.user.userId}/tickets`);
     return {
       tickets: tickets
+    }
+  },
+  filters: {
+    date(date) {
+      if (!date) return ''
+      date = dateFormat(date);
+      return date;
     }
   }
 }
@@ -42,6 +51,19 @@ export default {
 <style scoped>
   h1 {
     margin: 50px 0 50px 0;
+  }
+  .received {
+    display: block;
+    height: 20px;
+    width: 20px;
+    margin: auto;
+    border-radius: 999rem;
+  }
+  .success {
+    background-color: #43A047;
+  }
+  .not-yet {
+    background-color: #ddd;
   }
 
 </style>
