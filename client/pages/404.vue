97a6bf535a8a71039f9b60e94331c26ceef12448
Carlos <carlos.olmedodev@gmail.com>
2021-03-11 15:31:46 +0100

    feat(client, 404): Page not found done.

--- client/pages/404.vue
+++ client/pages/404.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="c-404">
-    <h1>404</h1>
-    <p>
-      <a href="/">Retourner vers la page d'accueil</a>
-    </p>
+    <div class="content">
+      <img class="img-logo-fatboar" src="~/assets/images/logo-fatboar.svg" alt="logo FatBoar">
+      <h1>404</h1>
+      <h2>Désolé, page non trouvée</h2><br />
+      <a class="c-btn c-btn--primary" href="/">Retourner vers la page d'accueil</a>
+    </div>
+    <div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  middleware: ['auth'],
-  auth: false
-}
+  export default {
+    layout: '404',
+    middleware: ['auth'],
+    auth: false
+  }
+
 </script>
 
 <style scoped>
@@ -19,6 +25,31 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
+    height: 80vh;
   }
+
+  .content {
+    text-align: center;
+  }
+
+  h1 {
+    margin-bottom: 1rem;
+  }
+
+  h2 {
+    margin-bottom: 2rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .img-logo-fatboar {
+    height: 50px;
+    width: 50px;
+    margin-bottom: 1.5rem;
+    background-color: #332879;
+    border-radius: 999rem;
+  }
+
 </style>
