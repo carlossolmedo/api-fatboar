b1adab623d4d2c9d14ed1620ee7385e813c9c295
Carlos <carlos.olmedodev@gmail.com>
2021-03-11 11:09:57 +0100

    feat(client, navbar): Styles responsive updated

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div class="c-container">
-      <div class="c-row c-row--middle">
+      <div class="c-row c-row--bottom">
         <div class="c-row_col c-row__col--auto">
           <div class="c-site-branding">
               <NuxtLink to="/" class="c-site-branding__title">
@@ -9,7 +9,7 @@
               </NuxtLink>
           </div>
         </div>
-        <div class="c-row__col c-row__col--auto u-hidden-sm u-hidden-md">
+        <div class="c-row__col c-row__col--auto">
           <Navbar v-if="$auth.loggedIn" />
         </div>
       </div>
