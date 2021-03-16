53622b20778dd2824e3def630b7d3e4b01b8dbfc
Carlos <carlos.olmedodev@gmail.com>
2021-03-16 17:17:58 +0100

    feat(client, admin): Icons added and buttons actions for tables updated - New components for lists

---
+++ client/components/admin/ListAdminAccounts.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="block-list-users">
+    <table class="table-fluid table-list-admin">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nom</th>
+          <th>Adresse mail</th>
+          <th>Rôle</th>
+          <th>Date de création</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Carlos Olmedo</td>
+          <td>carlos@fatboar.com</td>
+          <td>Admin</td>
+          <td>10/03/2021</td>
+          <td class="actions">
+            <a class="icon icon-red" title="modifier"><edit-2-icon size="16" class=""></edit-2-icon></a>
+            <a class="icon icon-red" title="supprimer"><trash-2-icon size="16" class=""></trash-2-icon></a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- <div>
+      {{ adminUsers }}
+    </div> -->
+  </div>
+</template>
+
+<script>
+import { Edit2Icon, Trash2Icon } from 'vue-feather-icons';
+
+export default {
+  props: {
+    adminUsers: Array
+  },
+  components: {
+    Edit2Icon,
+    Trash2Icon
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/components/admin/ListUserAccounts.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="block-list-users">
+    <table class="table-fluid table-list-users">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Genre</th>
+          <th>Nom</th>
+          <th>Adresse mail</th>
+          <th>Date de création</th>
+          <th>Récuperer</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>M</td>
+          <td>Carlos Olmedo</td>
+          <td>carlos@fatboar.com</td>
+          <td>10/03/2021</td>
+          <td><span class="received" :class="{'success': ticket.received, 'not-yet': !ticket.received}"></span></td>
+          <td class="actions">
+            <a class="icon icon-red" title="désactiver"><trash-2-icon size="16" class=""></trash-2-icon></a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- <div>
+      {{ users }}
+    </div> -->
+  </div>
+</template>
+
+<script>
+import { Edit2Icon, Trash2Icon } from 'vue-feather-icons';
+
+export default {
+  props: {
+    users: Array
+  },
+  components: {
+    Edit2Icon,
+    Trash2Icon
+  },
+  data() {
+    return {
+      ticket: {
+        received: false
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
