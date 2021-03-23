0159d2d54291ab7650425df3d6a3bda1f00a26a2
Carlos <carlos.olmedodev@gmail.com>
2021-03-23 13:01:42 +0100

    feat(client, admin): Generate winner big prize done.

--- client/components/admin/BigPrize.vue
+++ client/components/admin/BigPrize.vue
@@ -2,11 +2,16 @@
   <transition name="fade" mode="in-out" appear>
     <div id="panelBigPrize" class="panel">
       <h3>Générer gagnant grand prix</h3>
-      <div class="block-img-logo">
-        <img class="img-logo-prize" src="~/assets/images/logo-fatboar.svg" alt="logo FatBoar">
+      <div v-if="!loading" class="block-img-logo">
+        <img class="img-logo-prize" :class="{'spin-logo': spinLogo}" src="~/assets/images/logo-fatboar.svg" alt="logo FatBoar">
+      </div>
+      <div v-if="loading" class="winner">
+        <h2 class="title-winner">🎊 {{ winner.username }} 🎊</h2>
+        <h3>{{ winner.email }}</h3>
+        <h4>Prix : Range Rover Evoque</h4>
       </div>
       <div class="block-btn-submit">
-        <button @click="submit" class="c-btn c-btn--primary" type="button">Valider</button>
+        <button @click="getWinner" class="c-btn c-btn--primary" type="button">Valider</button>
       </div>
     </div>
   </transition>
@@ -14,9 +19,32 @@
 
 <script>
   export default {
+    data: () => ({
+      loading: false,
+      spinLogo: false,
+      winner: {
+        username: '',
+        email: ''
+      }
+    }),
     methods: {
-      submit() {
-        const panel = document.getElementById('panelBigPrize');
+      async getWinner() {
+        this.loading = false;
+        this.spinLogo = true;
+        await this.$axios.$get(`/user/big-prize`)
+        .then((user) => {
+          this.winner = {
+            username: user[0].username,
+            email: user[0].email
+          }
+          setTimeout(() => {
+            this.spinLogo = false;
+            this.loading = true;
+          }, 1000)
+        }).catch((err) => {
+          console.error(err);
+          this.$toast.error('Impossible de trouver le gagnant').goAway(3000);
+        });
       }
     }
   }
